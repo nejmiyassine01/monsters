2333c95252bdc9c06610cbb6cfb17fd74f22adcf
nejmiyassine01 <nejmiyassine01@gmail.com>
2020-09-17 14:16:04 +0100

    Updates

---
+++ precache-manifest.cd213b72cc5f7e3c5d6f7dd8ec7da099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918f55ab2ee4aa26763fa45f229a19b2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b97e3752a2ad3aa7d776",
+    "url": "/monsters/static/css/main.349d235f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97e3752a2ad3aa7d776",
+    "url": "/monsters/static/js/main.95813a77.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
